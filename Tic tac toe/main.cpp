b839efe9fbcf099f1f6badf1910e30ef0cda3ecd
Stefanos Mitropoulos <stefmitropoulos@gmail.com>
2018-03-28 18:55:58 +0300

    Initial Commit

---
+++ Tic tac toe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tic tac toe
+//
+//  Created by Stefanos Mitropoulos on 28/03/2018.
+//  Copyright © 2018 Stefanos Mitropoulos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
